b598e14b2d0ce4245cbc9086feaf09faf536f73d
sadraliev <a.sadraliev@gmail.com>
2024-11-12 13:19:49 -0500

    chore(app): Add formatter, linter, jest

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["**/*.{js, mjs, cjs}"] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  pluginJs.configs.recommended,
+  eslintPluginPrettierRecommended,
+  {
+    ignores: ["node_modules", "dist"],
+  },
+  {
+    rules: {
+      "no-console": "warn",
+    },
+  },
+];
